9a69e81e8e252691c2566d6912c5a0fae827e570
Emil Rapp <emilemail4@gmail.com>
2023-04-28 15:20:48 +0200

    tailwind , pretteier, and other fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 export const metadata = {
-  title: "Fysikboken",
-  icons: {
-    icon: "./favicon.png",
-  },
+    title: "Fysikboken",
+    icons: {
+        icon: "./favicon.png",
+    },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="sv">
-      <body>{children}</body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="sv">
+            <body>{children}</body>
+        </html>
+    );
 }
